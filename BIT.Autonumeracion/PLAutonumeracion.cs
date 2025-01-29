d05e450ae1deb941f87088e37b8d35daae82b29d
Sebastian Ochoa <sebastian.ochoa@grupobusiness.it>
2025-01-29 18:42:46 -0500

    init project

---
+++ BIT.Autonumeracion/PLAutonumeracion.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BIT.Autonumeracion
+{
+    public class PLAutonumeracion : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            IOrganizationServiceFactory factory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService service = factory.CreateOrganizationService(context.UserId);
+            IOrganizationService iServices = ((IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory))).CreateOrganizationService(new Guid?(context.UserId));
+            ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+            {
+                try
+                {
+                    /////Programar la logica
+                    ///
+
+
+
+
+
+
+                }
+                catch (Exception ex)
+                {
+
+                    throw new InvalidPluginExecutionException("Error plugin. " + ex.Message);
+                }
+            }
+
+        }
+    }
+}
